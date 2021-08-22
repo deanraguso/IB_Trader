67c75342801fdaa6f311243aaf27bb23f81cb317
deanr <deanr@DESKTOP-35EFB6S>
2021-08-22 18:48:12 +1000

    Add project files.

---
+++ IB_Trader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IB_Trader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
